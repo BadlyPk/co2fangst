5bc2cb9355e7b39e4f77041b22955ac90fd2cf3e
masafakadamaha <staale.breimoen@hotmail.com>
2021-04-22 12:19:58 +0200

    Update Tre-trinn.py

--- Tre-trinn.py
+++ Tre-trinn.py
@@ -17,7 +17,7 @@ gamma = cpCO2/(cpCO2-c.gasConst)
 T_inn1 = 298
 p_inn1 = 2
 p_ut1 = 4
-T_ut1 = T_inn1*(p_ut1/p_inn1)**((gamma-1)-gamma)
+T_ut1 = T_inn1*(p_ut1/p_inn1)**((gamma-1)/gamma)
 Ws_rev1 = m9*(c.cpg[0]*1000)*(T_ut1-T_inn1)
 Ws_real1 = Ws_rev1/c.eta
 To_real1 = T_ut1+(Ws_real1/(m9*(c.cpg[0]*1000)))
@@ -32,7 +32,7 @@ print("---")
 T_inn2 = 303
 p_inn2 = 4
 p_ut2 = 8
-T_ut2 = T_inn2*(p_ut2/p_inn2)**((gamma-1)-gamma)
+T_ut2 = T_inn2*(p_ut2/p_inn2)**((gamma-1)/gamma)
 Ws_rev2 = m9*(c.cpg[0]*1000)*(T_ut2-T_inn2)
 Ws_real2 = Ws_rev2/c.eta
 To_real2 = T_ut2+(Ws_real2/(m9*(c.cpg[0]*1000)))
@@ -47,7 +47,7 @@ print("---")
 T_inn3 = 303
 p_inn3 = 8
 p_ut3 = 20
-T_ut3 = T_inn3*(p_ut3/p_inn3)**((gamma-1)-gamma)
+T_ut3 = T_inn3*(p_ut3/p_inn3)**((gamma-1)/gamma)
 Ws_rev3 = m9*(c.cpg[0]*1000)*(T_ut3-T_inn3)
 Ws_real3 = Ws_rev3/c.eta
 To_real3 = T_ut3+(Ws_real3/(m9*(c.cpg[0]*1000)))
