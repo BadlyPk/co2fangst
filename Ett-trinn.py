b049dcf47aee19eb6c789fed2100b275c494fbe1
BadlyPk <martin.pham10@yahoo.no>
2021-04-22 12:17:27 +0200

    Push your mother

--- Ett-trinn.py
+++ Ett-trinn.py
@@ -6,27 +6,25 @@ Gruppe 6 - Andrea Skog, Ståle Breimoen, Hanna Imsland Mo, Martin Pham.
 
 """
 import numpy as np
+import constants6 as c
 
 T_inn = 298
-
 MmCO2 = 44.01
 p_inn = 2
 p_ut = 20
 T_ut_slutt = 303
 p_ut_slutt = 20
 
-cpCO2_ubrukt = 0.868*1000
-cpCO2 = ((cpCO2_ubrukt)/1000)*MmCO2
-m9 = 6
-eta = 0.85
+cpCO2 = (((c.cpg[0]*1000))/1000)*MmCO2
+m9 = 84.63
 R = 8.314
 gamma = cpCO2/(cpCO2-R)
 
 T_ut = T_inn*(p_ut/p_inn)**((gamma-1)/gamma)
-Ws_rev = m9*cpCO2_ubrukt*(T_ut-T_inn)
-Ws_real = Ws_rev/eta
-To_real = T_ut+(Ws_real/(m9*cpCO2_ubrukt))
-Q_kjoler = m9*cpCO2_ubrukt*(T_inn-To_real)
+Ws_rev = m9*(c.cpg[0]*1000)*(T_ut-T_inn)
+Ws_real = Ws_rev/c.eta
+To_real = T_ut+(Ws_real/(m9*(c.cpg[0]*1000)))
+Q_kjoler = m9*(c.cpg[0]*1000)*(T_inn-To_real)
 
-print(round(Ws_real/1000,3), "kJ/s")
-print(round(Q_kjoler/1000,2), "kJ")
+print("Ws_real:", round(Ws_real/1000,3), "kJ/s")
+print("Q_kjøler:", round(Q_kjoler/1000,2), "kJ")
